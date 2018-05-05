3e4f20117efdc229e822af3e7f0cba302b946ae1
sashaDoubov <doubovs@gmail.com>
2018-05-05 10:08:33 -0400

    removing extra file

--- testCamera.py
+++
@@ -1,8 +0,0 @@
-import picamera
-import picamera.array
-
-with picamera.PiCamera() as camera:
-    with picamera.array.PiRGBArray(camera) as output:
-        camera.resolution = (1280,720)
-        camera.capture(output, 'bgr')
-        print(output.array)
